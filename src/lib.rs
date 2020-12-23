f976df3574920c9de0be930d55ab5348fe247c69
vlakreeh <zeb@zebulon.dev>
2020-12-23 12:38:26 -0500

    Layout api

---
+++ src/lib.rs
@@ -0,0 +1,69 @@
+use anyhow::Result;
+use wasi_common::WasiCtx;
+use wasmtime::{Func, Linker, Store};
+
+/// An instantiated instance of the wasi exports.
+///
+/// This represents a wasi module which can be used to instantiate other wasm modules.
+/// This structure exports all that various fields of the wasi instance as fields which can be used to implement your own instantiation logic, if necessary.
+/// Additionally [`AutoWasi::get_export`](crate::AutoWasi::get_export) can be used to do name-based resolution.
+pub enum AutoWasi {
+    Snapshot0(wasmtime_wasi::old::snapshot_0::Wasi),
+    Snapshot1(wasmtime_wasi::Wasi),
+}
+
+impl AutoWasi {
+    /// Creates a new [`AutoWasi`](crate::AutoWasi) that allows for linking from the detected
+    /// wasi version.
+    pub fn detect<T: AsRef<[u8]>>(store: &Store, ctx: WasiCtx, binary: T) -> Result<Self> {
+        let version = WasiVersion::detect(binary)?;
+        Ok(Self::new(store, ctx, version))
+    }
+
+    /// Creates a new [`AutoWasi`](crate::AutoWasi) that allows for linking from the provided
+    /// [`WasiVersion`](crate::WasiVersion).
+    pub fn new(store: &Store, ctx: WasiCtx, version: WasiVersion) -> Self {
+        match version {
+            WasiVersion::Snapshot0 => {
+                let wasi = wasmtime_wasi::old::snapshot_0::Wasi::new(&store, ctx);
+                Self::Snapshot0(wasi)
+            }
+            WasiVersion::Snapshot1 => {
+                let wasi = wasmtime_wasi::Wasi::new(&store, ctx);
+                Self::Snapshot1(wasi)
+            }
+        }
+    }
+
+    /// Looks up a field called name in this structure, returning it if found.
+    /// This is often useful when instantiating a wasmtime instance where name resolution often happens with strings.
+    pub fn get_export(&self, name: &str) -> Option<&Func> {
+        match self {
+            Self::Snapshot0(wasi) => wasi.get_export(name),
+            Self::Snapshot1(wasi) => wasi.get_export(name),
+        }
+    }
+
+    /// Adds all instance items to the specified Linker.
+    pub fn add_to_linker(&self, linker: &mut Linker) -> Result<()> {
+        match self {
+            Self::Snapshot0(wasi) => wasi.add_to_linker(linker),
+            Self::Snapshot1(wasi) => wasi.add_to_linker(linker),
+        }
+    }
+}
+
+/// The version of WASI that a binary relies on.
+#[derive(Debug, Clone)]
+pub enum WasiVersion {
+    /// Called `wasi_unstable` in binaries.
+    Snapshot0,
+    /// Called `wasi_snapshot_preview1` in binaries.
+    Snapshot1,
+}
+
+impl WasiVersion {
+    pub fn detect<T: AsRef<[u8]>>(_binary: T) -> Result<Self> {
+        todo!()
+    }
+}
